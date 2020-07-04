faa7155d3adcff41587677eee30a1b71e7313d9d
Shawn Choi <shchoice0812@gmail.com>
2020-07-04 23:18:32 +0900

    information hiding example

---
+++ Java/InformationHiding/MyDateMain.java
@@ -0,0 +1,16 @@
+package InformationHiding;
+
+public class MyDateMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyDate date = new MyDate();
+		
+		date.setYear(2020);
+		date.setMonth(7);
+		date.setDay(4);
+		
+		date.showDate();
+	}
+
+}
